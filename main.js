ad1c32fa3a263a74ab4f07bf82e6849d1df7db54
soumabera <154750797+soumabera@users.noreply.github.com>
2024-09-06 01:00:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var typed= new typed(".text",{
+    Strings:["Frontend Developer","Youtuber","Web Devoloper"],
+    typespeed:100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+
+});
+
+var sidemenu = document.getElementById("sidemenu")
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
